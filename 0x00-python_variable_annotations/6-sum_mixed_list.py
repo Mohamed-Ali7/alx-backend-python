b129dd8e0f5aab752a9e9a4e086067d2f5b3ceea
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-02 01:15:32 +0300

    Adding 6-sum_mixed_list.py and 6-main.py files

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains sum_mixed_list() function"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """returns the sum of mixed list elements as a float."""
+
+    result: float = 0
+    for ele in mxd_lst:
+        result += ele
+
+    return result

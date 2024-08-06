0a1eab7c9d2661e8e89df82d78c3c1157d2b23e7
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-06 03:58:45 +0300

    Updating 1-concurrent_coroutines.py file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""This module contains wait_random() function (Coroutine)"""
+"""This module contains wait_n() function (Coroutine)"""
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Will spawn wait_random n times with the specified max_delay.
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order
     """
-    tasks: list = [wait_random(max_delay) for _ in range(n)]
-    delays: list = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
 
     delays = [await delay for delay in asyncio.as_completed(tasks)]
     return delays

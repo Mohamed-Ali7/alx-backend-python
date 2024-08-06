315e0f4a9cd820d0cb0aefba11ccf5d1691c3dd0
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-06 04:15:25 +0300

    Adding 4-tasks and 4-main.py files

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""This module contains wait_n() function (Coroutine)"""
-
-import asyncio
+"""This module contains task_wait_n() function (Coroutine)"""
 from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Will spawn wait_random n times with the specified max_delay.
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = [await delay for delay in asyncio.as_completed(tasks)]
     return delays

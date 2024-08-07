12b161674526336d44bb96499ae0d142740d3879
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-08 02:25:54 +0300

    Updating 2-measure_runtime.py file

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,12 +13,12 @@ async def measure_runtime() -> float:
     Executes async_comprehension four times in parallel using asyncio.gather.
     measure_runtime should measure the total runtime and return it.
     """
-    start_time = time.time()
+    start_time = asyncio.get_event_loop().time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time.time()
+    end_time = asyncio.get_event_loop().time()
     return end_time - start_time
